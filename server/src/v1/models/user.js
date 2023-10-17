7d8eeccc3ffe01d0f0f3f85b1a5333625a28a6fb
Harzhedzmi Gani <harzhedzmig@gmail.com>
2023-10-18 04:50:32 +0800

    Feat tokenHandler, controllers

--- server/src/v1/models/user.js
+++ server/src/v1/models/user.js
@@ -4,3 +4,24 @@ MONGODB_URL=mongodb+srv://kanban-app:oldspeeddemon112408kiankie123@kanbancluster
 PASSWORD_SECRET_KEY=GK255jx8HDZhxCSu
 TOKEN_SECRET_KEY=ockas@SSSNCA$1231
 */
+
+const mongoose = require("mongoose");
+const { schemaOptions } = require("./modelOptions");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+  },
+  schemaOptions
+);
+
+module.exports = mongoose.model("User", userSchema);
