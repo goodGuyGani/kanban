102bfddb0d12771a4a478c3e6b137ad5263370ed
Harzhedzmi Gani <harzhedzmig@gmail.com>
2023-10-18 03:09:32 +0800

    Save .env

---
+++ server/src/v1/models/user.js
@@ -0,0 +1,6 @@
+/*
+PORT=5000
+MONGODB_URL=mongodb+srv://kanban-app:oldspeeddemon112408kiankie123@kanbancluster.jsyqjnx.mongodb.net/?retryWrites=true&w=majority
+PASSWORD_SECRET_KEY=GK255jx8HDZhxCSu
+TOKEN_SECRET_KEY=ockas@SSSNCA$1231
+*/
