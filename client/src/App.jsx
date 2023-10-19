03f28c0e948565235ce9540181e058309d9653a0
Harzhedzmi Gani <harzhedzmig@gmail.com>
2023-10-20 04:33:13 +0800

    Frontend Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,19 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import CssBaseLine from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AppLayout from "./components/AppLayout";
+import AuthLayout from "./components/AuthLayout";
+import Home from "./pages/Home";
+import Board from "./pages/Board";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
-  return (
-    <div>
-      Kanban App
-    </div>
-  );
+  return <div>Kanban App</div>;
 }
 
 export default App;
